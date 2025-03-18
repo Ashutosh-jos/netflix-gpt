4b258ecc05fa8e03bbc85d165f5c6b7a61f1b0a6
Ashutosh-jos <jashutosh2016@gmail.com.com>
2025-03-18 11:48:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Ashutosh 
+      <h1 className="text-3xl font-bold underline text-rose-400">
+      Hello world!
+    </h1>
+    </div>
+
+
+  );
+}
+
+export default App;
